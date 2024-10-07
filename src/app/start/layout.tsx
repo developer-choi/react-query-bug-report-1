62dfcf1be94f97cc0fd048bcbf4961772d4967d3
choeyujin <yujin.choe@langdy.net>
2024-10-07 10:17:19 +0900

    Add example demonstrating bug occurrence

--- src/app/start/layout.tsx
+++ src/app/start/layout.tsx
@@ -1,22 +1,15 @@
 'use client';
 
 import {PropsWithChildren} from 'react';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {QueryClientProvider} from '@tanstack/react-query';
+import {getQueryClient} from '@/others/react-query';
 
 export default function Layout({children}: PropsWithChildren) {
+  const queryClient = getQueryClient()
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}
     </QueryClientProvider>
   );
 }
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      // With SSR, we usually want to set some default staleTime
-      // above 0 to avoid refetching immediately on the client
-      staleTime: 60 * 1000,
-    }
-  }
-});
--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import Link from 'next/link';
 import ClientListPage from '@/others/ClientListPage';
+import {dehydrate, HydrationBoundary, QueryClient} from '@tanstack/react-query';
+import {getListApi} from '@/others/list-api';
 
-export default function Page() {
+export default async function Page() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: ['list'],
+    queryFn: ({pageParam}) => getListApi(pageParam),
+    initialPageParam: 1
+  });
+
   return (
     <>
       <Link href="/start/next" style={{position: 'fixed', top: 0, background: 'red'}}>Move next page</Link>
-      <ClientListPage />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <ClientListPage />
+      </HydrationBoundary>
     </>
   );
 }
